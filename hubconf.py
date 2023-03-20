bd0308b0b03accac705a56e7823431498ea20254
lbleal1 <lowwwiis@gmail.com>
2023-03-20 15:51:07 +0000

    change model path

--- hubconf.py
+++ hubconf.py
@@ -1,4 +1,4 @@
-dependencies = ['torch']
+import torch
 
 def custom_model():
     """ # This docstring shows up in hub.help()
@@ -6,7 +6,7 @@ def custom_model():
     and returns the model 
     """
     # load
-    path = 'torch_hub\iris_classifier.pt'
+    path = 'load_custom_model\iris_classifier.pt'
     model = torch.load(path)
     return model
 
