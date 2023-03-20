ccbb0afb4f96c5e593a7e6fa34a8fd4dee6ea532
lbleal1 <lowwwiis@gmail.com>
2023-03-20 16:20:21 +0000

    changed model path

--- hubconf.py
+++ hubconf.py
@@ -8,7 +8,7 @@ def custom_model():
     and returns the model 
     """
     # load
-    path = 'load_custom_model\iris_classifier.pt'
+    path = './load_custom_model/iris_classifier.pt'
     model = torch.load(path)
     return model
 
