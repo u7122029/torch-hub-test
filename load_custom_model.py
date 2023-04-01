575d8b7f81bcd3f371680cfa609182a29a005557
lbleal1 <lowwwiis@gmail.com>
2023-04-01 11:03:03 +0100

    change folder structure

--- load_custom_model.py
+++ load_custom_model.py
@@ -1,6 +1,6 @@
 import torch
 #from model import Model
 
-model = torch.hub.load('lbleal1/torch-hub-test/custom_model', 
+model = torch.hub.load('lbleal1/torch-hub-test', 
                        'custom_model')
 print(model)
